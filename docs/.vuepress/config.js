03f1478c8c335c015377ec9fc13b3153612b8eec
Dennys Matthew <27568445+mclemie@users.noreply.github.com>
2022-05-08 12:03:13 +0700

    genesis

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+const { defaultTheme } = require('@vuepress/theme-default')
+const { docsearchPlugin } = require('@vuepress/plugin-docsearch')
+
+module.exports = {
+    lang: 'en-US',
+    title: 'Peradaban',
+    description: 'Proprietary bot for the Discord server of the Warung International.',
+    theme: defaultTheme({
+        logo: '/hero.png',
+        repo: 'warung-international/docs',
+        docsBranch: 'master',
+        docsDir: 'docs',
+    }),
+    plugins: [
+        docsearchPlugin({
+            appId: '2R49JO3IMP',
+            apiKey: 'cd47d4f87f4efc86fa4c440fccb19151',
+            indexName: 'docs',
+        }),
+    ],
+}
